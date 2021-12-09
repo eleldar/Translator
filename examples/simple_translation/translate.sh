3243036a9355b85129b611d8e58d3ed019bb917b
eldar <eleldar@mail.ru>
2021-12-09 16:02:06 +0300

    notations

--- examples/simple_translation/translate.sh
+++ examples/simple_translation/translate.sh
@@ -3,5 +3,5 @@
 # в файл output.de (содержит такие же разметки, как в файле input.en);
 # используется модель model.npz (RNN) и словарь vocab.spm
 ~/marian/build/marian-decoder -m model.npz -v vocab.spm < data/input.en vocab.spm > output.de 
-# расскоментируйсте следующую строку для получения аналогичного результата
+# расскоментируйте следующую строку для получения аналогичного результата
 # cat data/input.en | ~/marian/build/marian-decoder -m model.npz -v vocab.spm vocab.spm > output.de 
