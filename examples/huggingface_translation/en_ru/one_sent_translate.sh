51816898644413e38b0869ebf76930ef2553d11f
eldar <eleldar@mail.ru>
2021-12-14 00:20:32 +0300

    one sentence translation with preprocessing

--- examples/huggingface_translation/en_ru/one_sent_translate.sh
+++ examples/huggingface_translation/en_ru/one_sent_translate.sh
@@ -3,5 +3,5 @@ NMT=~/marian/build
 SPM=./data/source.spm
 VOCAB=./model/opus.spm32k-spm32k.vocab.yml
 MODEL=./model/opus.spm32k-spm32k.transformer-align.model1.npz.best-perplexity.npz
-${NMT}/spm_encode --model ${SPM} $1 |
+./preprocess.sh $1 |
 ${NMT}/marian-decoder -m ${MODEL} -v ${VOCAB} ${VOCAB} | ./postprocess.sh 
--- examples/huggingface_translation/en_ru/preprocess.sh
+++ examples/huggingface_translation/en_ru/preprocess.sh
@@ -5,26 +5,14 @@
 #
 # replace MOSESHOME and SPMENCODE with your own setup! 
 
-if [ `hostname -d` == "bullx" ]; then
-  APPLHOME=/projappl/project_2001569
-  MOSESHOME=${APPLHOME}/mosesdecoder
-  SPMENCODE=${APPLHOME}/marian-dev/build-spm/spm_encode
-elif [ `hostname -d` == "csc.fi" ]; then
-  APPLHOME=/proj/memad/tools
-  MOSESHOME=/proj/nlpl/software/moses/4.0-65c75ff/moses
-  SPMENCODE=${APPLHOME}/marian-dev/build-spm/spm_encode
-else
-  MOSESHOME=${PWD}/mosesdecoder
-  SPMENCODE=${PWD}/marian-dev/build/spm_encode
-fi
+SPMENCODE=~/marian/build/spm_encode
+TOKENIZER=~/marian/marian-examples/tools/moses-scripts/scripts/tokenizer
+SPM=./data/source.spm
 
-MOSESSCRIPTS=${MOSESHOME}/scripts
-TOKENIZER=${MOSESSCRIPTS}/tokenizer
-
-
+cat $1 |
 ${TOKENIZER}/replace-unicode-punctuation.perl |
 ${TOKENIZER}/remove-non-printing-char.perl |
 sed 's/  */ /g;s/^ *//g;s/ *$//g' |
-${SPMENCODE} --model $2
+${SPMENCODE} --model ${SPM}
 
 # ${TOKENIZER}/normalize-punctuation.perl -l $1 |
