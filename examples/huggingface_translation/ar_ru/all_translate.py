567e4b26709d76ec66eaad51e0737f85feecbea7
eldar <eleldar@mail.ru>
2021-12-16 19:31:38 +0300

    python scripts for all arabic sentences; reports data

---
+++ examples/huggingface_translation/ar_ru/all_translate.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+import subprocess
+import os
+
+source = 'input.ar'
+target = 'output.ru'
+with open(source, encoding='utf-8') as f:
+    file = f.readlines()
+if os.path.exists(target):
+    os.remove(target)
+for sent in [i.strip() for i in file]:
+    command = f'echo "{sent}" | ./translate.sh >> {target}'
+    os.system(command)
