7df28b50900b2c8a33c0f5349a646409177ed0a2
eldar <eleldar@mail.ru>
2022-06-06 10:43:40 +0300

    argparse port

--- OpenAPI/main.py
+++ OpenAPI/main.py
@@ -1,6 +1,7 @@
 # main.py
 import os
 from flask import Flask
+from argparse import ArgumentParser
 from documented_endpoints import blueprint as documented_endpoint
 
 app = Flask(__name__)
@@ -9,5 +10,12 @@ app.config['RESTPLUS_MASK_SWAGGER'] = False
 app.register_blueprint(documented_endpoint)
 
 if __name__ == "__main__":
+    default_port = '8888'
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-p", "--port", dest="port", default=default_port,
+        help=f'Enter connect port; default="{default_port}"'
+    )
+    args = parser.parse_args()
     os.environ["TRANSFORMERS_OFFLINE"] = "1"
-    app.run(host='0.0.0.0', port='8888')
+    app.run(host='0.0.0.0', port=args.port)
